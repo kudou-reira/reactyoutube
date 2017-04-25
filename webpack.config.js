373c645ee26a307e959f6cdb54c40a748f09274a
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-04-25 13:11:50 +0900

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: "./build",
-    publicPath: "./build",
+    path: __dirname,
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
